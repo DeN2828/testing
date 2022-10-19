4414cf8bc3354c905866da38a7f1162ed6cdb879
DeN2828 <den-tu@ua.fm>
2022-10-19 10:08:21 +0300

    change string input1

--- zood/zood/Program.cs
+++ zood/zood/Program.cs
@@ -5,6 +5,6 @@ string input = Console.ReadLine();
 Moon moon = new Moon(input);
 moon.Output();
 Console.WriteLine("enter birth date in format DD.MM");
-string input = Console.ReadLine();
-Zood zood = new Zood(input);
+string input1 = Console.ReadLine();
+Zood zood = new Zood(input1);
 zood.Output();
